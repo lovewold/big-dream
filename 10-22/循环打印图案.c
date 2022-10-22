6cae78833fb3b8321fcebca21728d9003e575358
lovewold_ <110475721+lovewold@users.noreply.github.com>
2022-10-22 21:35:52 +0800

    10-22

---
+++ 10-22/循环打印图案.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+// 正方形图案
+// #include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//	int b = 0;
+//
+//	scanf("%d", &i);
+//	for (a = 0; a < i; a++)
+//	{
+//		for (b = 0; b < i; b++)
+//		{
+//			printf("* ");
+//		}printf("\n");
+//	}
+//	return 0;
+//}
+////多组输入，直角三角形图案
+//#include <stdio.h>
+//
+//int main() {
+//    int a, b, c;
+//    while (scanf("%d", &a) != EOF) {
+//        for (b = 1; b <= a; b++)
+//        {
+//            for (c = 1; c <= b; c++)
+//            {
+//                printf("* ");
+//            }printf("\n");
+//        }
+//
+//
+//    }
+//    return 0;
+//}
+//反转三角形
+//#include <stdio.h>
+//
+//int main() {
+//    int a = 0;
+//    int i = 0;
+//    int b = 0;
+//    while (scanf("%d", &a) != EOF)
+//    {
+//        for (i = 1; i <= a; i++)
+//        {
+//            for (b = 0; b < a - (i - 1); b++)
+//            {
+//                printf("* ");
+//            }printf("\n");
+//        }
+//    }
---
+++ 10-22/网购.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	float a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	scanf("%f %d %d %d", &a, &b, &c, &d);
+//	
+//	if (b == c && b == 11 )
+//	{
+//		if (d == 1)
+//		{
+//			if (a * 0.7 - 50 >= 0)
+//			{
+//				printf("%.2f", a * 0.7 - 50);
+//			}
+//		    else
+//		    {
+//				printf("0.00\n");
+//		    }
+//		}	
+//		    
+//		else if (d == 0)
+//		{
+//			printf("%.2f", a * 0.7);
+//		}
+//			
+//		
+//	}
+//	else if (b == c && b == 12 )
+//	{
+//		if (d == 1)
+//		{
+//			if (a * 0.8 - 50 >= 0)
+//			{
+//				printf("%.2f", a * 0.8 - 50);
+//			}
+//			else
+//			{
+//				printf("0.00\n");
+//			}
+//			
+//		}
+//			
+//		else if (d == 0)
+//		{
+//			printf("%.2f", a * 0.8);
+//		}
+//			
+//	}
+//	else 
+//	{
+//		printf("%.2f", a);
+//	}
+//	
+//	
+//
+//	return 0;
+//
+//}
