34ccc71d451b052a9d313017ba6820f8bb98f086
lovewold_ <110475721+lovewold@users.noreply.github.com>
2022-10-29 00:44:18 +0800

    蓝桥杯

---
+++ Project3/源.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//    int n = 1;
+//    int m = 2;
+//    switch (n)
+//    {
+//    case 1: m++;
+//    case 2: n++;
+//    case 3:
+//        switch (n)
+//        {//switch‘ –Ì«∂Ã◊ π”√
+//        case 1: n++;
+//        case 2:
+//            m++; n++;
+//            break;
+//        }
+//    case 4:
+//        m++;
+//        break;
+//    default:
+//        break;
+//    }
+//    printf("m = %d, n = %d\n", m, n);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	float a = 0;
+//	double b = 0;
+//	long double c = 0;
+//	float ave = 0;
+//	scanf("%f %lf %lf", &a, &b, &c);
+//	ave = (a + b + c) / 3;
+//	printf("%f",ave);
+//	return 0;
