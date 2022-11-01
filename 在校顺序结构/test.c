2248f9bd1ee9cef70bdec4ef6e24ddfb02e74369
lovewold_ <110475721+lovewold@users.noreply.github.com>
2022-11-01 23:42:39 +0800

    11_1

---
+++ 在校顺序结构/test.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//	float r = 0;
+//	float pai = 3.14;
+//	scanf("%f", &r);
+//	float acre = 0;
+//	acre = r * r * pai;
+//	printf("%.2f", acre);
+//	return 0;
+//}
+// 
+// 
+//#include<stdio.h>
+//int main()
+//{
+//	int F = 0;
+//	double c = 0;
+//	int t = 0;
+//	scanf("%d", &F);
+//	c = 5.0 / 9 * (F - 32);
+//	printf("%.1f", c);
+//
+//	return 0;
+//	
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	if (n % 2 != 0)
+//	{
+//		printf("%d是奇数\n",n);
+//	}
+//	else
+//	{
+//		printf("%d是偶数\n",n);
+//	}
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	float n = 0;
+//	float lilv = 0;
+//	scanf("%f %f", &n,&lilv);//输入总金额和给定的利率（单位%已给）
+//	int i = 0;
+//	int sum = 0;
+//	for (i = 0; i < 3; i++)//计算第三年的
+//	{
+//		n=n * (1 + lilv / 100.0);
+//	}
+//	printf("%f\n", n);
+//
+//	return 0;
+//}
+//整型变量m=97，n=98，请编程将m和n分别以整型(int)和字符型(char)输出。
+//#include<stdio.h>
+//int main()
+//{
+//	int m = 97;
+//	int n = 98;
+//	printf("%d %c", m, n);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	char arr[100] = { 0 };
+//	scanf("%s", arr);
+//	int i = 0;
+//	while (arr[i] != '\0')
+//	{
+//		printf("%c", arr[i]-32);
+//		i++;
+//	}
+//	
+//
+//}
---
+++ 递归/my_strlen.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+////·Çµİ¹é
+//#include<stdio.h>
+//int my_strlen(char* x)
+//{
+//	int count = 0;
+//	while(*x != '\0')
+//	{
+//		count++;
+//		x++;
+//
+//	}
+//	return count;
+//}
+////µİ¹éÊµÏÖ
+//int my_strlen1(char* x)
+//{
+//	if (*x != '\0')
+//	{
+//		return 1 + my_strlen1(x++);
+//	}
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char arr[]="adbbh";
+//	int ret = my_strlen(arr);
+//	int ter = my_strlen(arr);
+//	printf("%d %d", ret,ter);
+//	return 0;
+//}
