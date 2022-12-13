bb12879001f8dbaad122bc69b20109e8da9800a6
lovewold_ <110475721+lovewold@users.noreply.github.com>
2022-12-13 20:53:58 +0800

    创建 源.c

---
+++ Project7/源.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+void cat(char* s, char* t)
+{
+	while (*s != '\0')
+		s++;
+	while ((*s++ = *t++) != '\0');
+}
+void main()
+{
+	char s1[80] = "world!", s2[80] = "Hello_";
+	cat(s2, s1);
+	printf("%s\n", s2);
+}
+
