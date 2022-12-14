4000c1e4b369133192d9c65fc1aa46c800e2fd03
lovewold_ <110475721+lovewold@users.noreply.github.com>
2022-12-14 22:25:20 +0800

    更新 源.c

--- Project7/源.c
+++ Project7/源.c
@@ -1,16 +1,38 @@
-
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-void cat(char* s, char* t)
-{
-	while (*s != '\0')
-		s++;
-	while ((*s++ = *t++) != '\0');
-}
-void main()
-{
-	char s1[80] = "world!", s2[80] = "Hello_";
-	cat(s2, s1);
-	printf("%s\n", s2);
-}
-
+//#include<stdio.h>
+//void cat(char* s, char* t)
+//{
+//	while (*s != '\0')
+//		s++;
+//	while ((*s++ = *t++) != '\0');
+//}
+//void main()
+//{
+//	char s1[80] = "world!", s2[80] = "Hello_";
+//	cat(s2, s1);
+//	printf("%s\n", s2);
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//    int i, j, a;
+//    void del(char* str, char c);
+//    char str[100], c;
+//    printf("ÇëÊäÈë×Ö·û´®:\n");
+//    gets(str);
+//    printf("ÇëÊäÈëÐèÒªÉ¾³ýµÄ×Ö·û:\n");
+//    scanf("%c", &c);
+//    for (a = 0; str[a] != '\0'; a++)
+//        for (i = a; i >= 0; i--)
+//        {
+//            if (str[i] == c)
+//            {
+//                for (j = i; j <= a + 1; j++)
+//                    str[j] = str[j + 1];
+//            }
+//        }
+//    printf("É¾³ýºóµÄ×Ö·û´®:");
+//    puts(str);
+//    return 0;
+//}
