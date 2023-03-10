8d88172eb5338500297b2c167c940729229a680f
lovewold_ <110475721+lovewold@users.noreply.github.com>
2023-03-11 02:09:51 +0800

    创建 剪枝.c

---
+++ Project13/剪枝.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	//输入n棵树
+//	int tree = 0;
+//	scanf("%d", &tree);
+//	int arr[10000] = { 0 };
+//	int i = 0;
+//	int n = tree-1;
+//	for (i = 0; i < tree; i++)
+//	{
+//		arr[i] = (tree - (i + 1)) * 2;
+//
+//	}
+//	int j = 0;
+//	while (j++<n--)
+//	{
+//		arr[n+1]= arr[j-1];
+//	}
+//	for (i = 0; i < tree; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//	//输出每棵树最多长多高
+//	return 0;
+//
+//}
+#include<stdio.h>
+
+int main()
+{
+	//创建二维数组；
+	int N, M, K = 0;
+	scanf("%d %d %d", &N, &M, &K);
+	int arr[500][500];
+	int i = 0;
+	int count = 1;
+	for (i = 0; i < N; i++)
+	{
+		int j = 0;
+		for (j = 0; j < M; j++)
+		{
+			arr[i][j] = count++;
+		}
+	}
+	int* p = 0;
+	for (i = 0; i < N; i++)//找子矩阵n*m;
+	{
+		int j = 0;
+		for (j = 0; j < M; j++)
+		{
+			int n = 0;
+			for (n = 0; n < j; n++) 
+			{
+
+			}
+		}
+	}
+	return 0;
+}
