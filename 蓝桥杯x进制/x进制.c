52aaceb3f0fd61142c8fcc6c7b89a0c7c20f78a9
lovewold_ <110475721+lovewold@users.noreply.github.com>
2023-03-17 00:47:23 +0800

    更新 x进制.c

--- 蓝桥杯x进制/x进制.c
+++ 蓝桥杯x进制/x进制.c
@@ -1,1 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+
+
+	int N,Ma = 0;
+	int ma[100000] = { 0 };
+	int Mb = 0;
+	int mb[100000] = { 0 };
+	
+	scanf("%d", &N);
+	scanf("%d", &Ma);
+	int i = 0;
+	for (i = 0; i < ma; i++)
+	{
+		scanf("%d ", &ma[i]);
+	}
+	
+	scanf("%d", &Mb);
+	for (i = 0; i < Mb; i++)
+	{
+		scanf("%d ", &mb[i]);
+	}
+
+
+	return 0;
+}
