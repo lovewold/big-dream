ad86ac5cc67d9bea95334dc6a687b48b0a14f6a5
lovewold_ <110475721+lovewold@users.noreply.github.com>
2023-10-08 13:21:19 +0800

    hello

---
+++ 二叉树/Tree.c
@@ -0,0 +1,110 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Tree.h"
+BTNode* BinaryTreeCreate(BTDataType* a, int n, int* pi)
+{
+	if (a[*pi] == '#')
+	{
+		(*pi)++;
+		return NULL;
+	}
+	BTNode* newNode = (BTNode*)malloc(sizeof(BTNode));
+	
+	newNode->data = a[*pi];
+	(*pi)++;
+	newNode->left = BinaryTreeCreate(a, n, pi);
+	newNode->right = BinaryTreeCreate(a, n, pi);
+	return newNode;
+}
+void BinaryTreeDestory(BTNode** root)
+{
+	if ((*root) == NULL)
+	{
+		return;
+	}
+	BinaryTreeDestory(&((*root)->left));
+	BinaryTreeDestory(&((*root)->right));
+	free(*root);
+	*root = NULL;
+}
+int BinaryTreeSize(BTNode* root)
+{
+	return root == NULL ? 0:BinaryTreeSize(root->left)+BinaryTreeSize(root->right)+1;
+}
+int BinaryTreeLeafSize(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	return root->left== NULL && root->right == NULL ? 1: BinaryTreeLeafSize(root->left) + BinaryTreeLeafSize(root->right);
+}
+int BinaryTreeLevelKSize(BTNode* root, int k)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	if (k == 1)
+	{
+		return 1;
+	}
+	return BinaryTreeLevelKSize(root->left, k - 1) + BinaryTreeLevelKSize(root->right, k - 1);
+}
+BTNode* BinaryTreeFind(BTNode* root, BTDataType x)
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	if (root->data == x)
+	{
+		return root;
+	}
+	BTNode* ret = NULL;
+	ret = BinaryTreeFind(root->left, x);
+	if (ret)
+	{
+		return ret;
+	}
+	ret = BinaryTreeFind(root->right, x);
+	if (ret)
+	{
+		return ret;
+	}
+	return NULL;
+}
+void BinaryTreePrevOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	printf(" %c ", root->data);
+	BinaryTreePrevOrder(root->left);
+	BinaryTreePrevOrder(root->right);
+}
+void BinaryTreeInOrder(BTNode* root) 
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	BinaryTreeInOrder(root->left);
+	printf(" %c ", root->data);
+	BinaryTreeInOrder(root->right);
+
+}
+void BinaryTreePostOrder(BTNode* root)
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	BinaryTreePostOrder(root->left);
+	BinaryTreePostOrder(root->right);
+	printf(" %c ", root->data);
+
+}
---
+++ 二叉树/test.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Tree.h"
+int main()
+{
+	BTDataType arr[] = "abc##de#g##f###";
+	int i = 0;
+	BTNode* newnode = BinaryTreeCreate(arr,sizeof(arr)/sizeof(BTDataType)-1,&i);
+	
+	printf("节点 %d\n", BinaryTreeSize(newnode));
+	printf("叶子节点 %d\n", BinaryTreeLeafSize(newnode));
+	printf("第k层节点 %d\n", BinaryTreeLevelKSize(newnode,5)); 
+	BTNode* tmp = BinaryTreeFind(newnode, 'f');
+	printf("%c 字符",tmp->data);
+	printf("\n前序遍历：");
+	BinaryTreePrevOrder(newnode);
+	printf("\n中序遍历：");
+	BinaryTreeInOrder(newnode);
+	printf("\n后序遍历：");
+	BinaryTreePostOrder(newnode);
+
+	BinaryTreeDestory(&newnode);
+	
+
+	return 0;
+}
